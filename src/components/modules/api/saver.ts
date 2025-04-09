938090997aa831e2c1500b4fa7942b0b93f24efc
sancasy <philip.a.eriksson@hotmail.com>
2025-04-09 15:05:50 +0200

    Update saver.ts

--- src/components/modules/api/saver.ts
+++ src/components/modules/api/saver.ts
@@ -15,7 +15,7 @@ export default class SaverAPI extends Module {
    */
   public get methods(): Saver {
     return {
-      save: (): Promise<OutputData> => this.save(),
+      save: (force=false): Promise<OutputData> => this.save(force),
     };
   }
 
@@ -24,10 +24,10 @@ export default class SaverAPI extends Module {
    *
    * @returns {OutputData}
    */
-  public save(): Promise<OutputData> {
+  public save(force=false): Promise<OutputData> {
     const errorText = 'Editor\'s content can not be saved in read-only mode';
-
-    if (this.Editor.ReadOnly.isEnabled) {
+    console.log("saving");
+    if (force == false && this.Editor.ReadOnly.isEnabled) {
       _.logLabeled(errorText, 'warn');
 
       return Promise.reject(new Error(errorText));
